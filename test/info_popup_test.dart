22fd3f1b8464d9adbfed9e2bd22f2babee87074b
Salih Can <salihcn.bnb@gmail.com>
2022-09-18 12:13:01 +0300

    refactor:

--- test/info_popup_test.dart
+++ test/info_popup_test.dart
@@ -4,7 +4,7 @@ import 'package:info_popup/info_popup.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
+    final Calculator calculator = Calculator();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
