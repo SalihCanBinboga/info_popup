14ef530b817bd8377e54f96f4a7ae8ee9e6e2ab7
Salih Can <salihcn.bnb@gmail.com>
2022-09-18 19:14:05 +0300

    feat: create on area pressed and on controller created typedef

---
+++ lib/src/typedefs/on_area_pressed.dart
@@ -0,0 +1,4 @@
+import 'package:info_popup/src/controllers/info_popup_controller.dart';
+
+/// [OnAreaPressed] is called when the tap area is pressed.
+typedef OnAreaPressed = void Function(InfoPopupController controller);
---
+++ lib/src/typedefs/on_controller_created.dart
@@ -0,0 +1,4 @@
+import 'package:info_popup/info_popup.dart';
+
+/// [OnControllerCreated] is called when the [InfoPopupController] is created.
+typedef OnControllerCreated = void Function(InfoPopupController controller);
