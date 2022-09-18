6921c3b52c27ef24c38e870634a67ec9e0dda5bd
Salih Can <salihcn.bnb@gmail.com>
2022-09-18 12:19:51 +0300

    feat: create info popup skeleton

--- lib/info_popup.dart
+++ lib/info_popup.dart
@@ -1,7 +1,3 @@
 library info_popup;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/info_popup_manager.dart';
---
+++ lib/src/info_popup_manager.dart
@@ -0,0 +1,5 @@
+/// Popup manager for the InfoPopup widget.
+/// [InfoPopupManager] is used to show and dismiss the popup.
+class InfoPopupManager {
+
+}
