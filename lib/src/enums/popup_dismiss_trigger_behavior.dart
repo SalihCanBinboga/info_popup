5fa2d3468aa23fd08a6f9f6b189fb2ef7098a746
Salih Can <salihcn.bnb@gmail.com>
2022-10-19 23:05:54 +0300

    feat: add dismiss behavior to info popup

---
+++ lib/src/enums/popup_dismiss_trigger_behavior.dart
@@ -0,0 +1,8 @@
+/// [PopupDismissTriggerBehavior] is used to specify the trigger behavior of the info popup.
+enum PopupDismissTriggerBehavior {
+  /// [onTapContent] is used to show the popup when the content is tapped.
+  onTapContent,
+
+  /// [onTapArea] is used to show the popup when the area outside the popup is tapped.
+  onTapArea,
+}
--- lib/src/enums/popup_trigger_behavior.dart
+++
@@ -1,8 +0,0 @@
-/// [PopupTriggerBehavior] is used to specify the trigger behavior of the info popup.
-enum PopupTriggerBehavior {
-  /// [onTap] is used to trigger the popup on tap.
-  onTap,
-
-  /// [manuel] is used to trigger the popup manually with controller.
-  manuel
-}
