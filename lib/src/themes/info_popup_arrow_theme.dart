f9ab4d2cb8ed3ff32531858fb11fd5368dc0a906
Salih Can <salihcn.bnb@gmail.com>
2022-10-19 23:07:14 +0300

    refactor: remove `arrowGap` and `arrowAlignment` props

--- lib/src/themes/info_popup_arrow_theme.dart
+++ lib/src/themes/info_popup_arrow_theme.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart' show Color, Colors, CustomPainter, Size;
 import 'package:info_popup/src/constants/popup_constants.dart';
 import 'package:info_popup/src/enums/arrow_direction.dart';
 
-import '../enums/arrow_alignment.dart';
-
 /// [InfoPopupArrowTheme] is used to customize the arrow of the popup.
 class InfoPopupArrowTheme {
   /// [InfoPopupArrowTheme] is creates a [InfoPopupArrowTheme] constructor.
@@ -15,8 +13,6 @@ class InfoPopupArrowTheme {
     this.arrowSize = PopupConstants.defaultArrowSize,
     this.arrowDirection = ArrowDirection.up,
     this.color = Colors.black,
-    this.arrowGap = 0,
-    this.arrowAlignment = ArrowAlignment.center,
     this.arrowPainter,
   });
 
@@ -29,12 +25,6 @@ class InfoPopupArrowTheme {
   /// The [color] of the arrow indicator.
   final Color color;
 
-  /// The [arrowGap] add a gap between the arrow and the content.
-  final double arrowGap;
-
-  /// The [arrowAlignment] of the arrow indicator.
-  final ArrowAlignment arrowAlignment;
-
   /// The [arrowPainter] is used to draw the arrow indicator.
   final CustomPainter? arrowPainter;
 
@@ -43,14 +33,12 @@ class InfoPopupArrowTheme {
     Size? arrowSize,
     ArrowDirection? arrowDirection,
     Color? color,
-    double? arrowGap,
     CustomPainter? arrowPainter,
   }) {
     return InfoPopupArrowTheme(
       arrowSize: arrowSize ?? this.arrowSize,
       arrowDirection: arrowDirection ?? this.arrowDirection,
       color: color ?? this.color,
-      arrowGap: arrowGap ?? this.arrowGap,
       arrowPainter: arrowPainter ?? this.arrowPainter,
     );
   }
