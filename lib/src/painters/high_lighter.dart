f11c37e96562c4e288a3ccf0a187e4200b41939c
Salih Can <salihcn.bnb@gmail.com>
2022-11-11 08:20:28 +0300

    feat: create high light painter

---
+++ lib/src/painters/high_lighter.dart
@@ -0,0 +1,42 @@
+
+part of '../controllers/info_popup_controller.dart';
+
+class _HighLighter extends CustomClipper<Path> {
+  _HighLighter({
+    this.area = Rect.zero,
+    this.radius = BorderRadius.zero,
+    this.padding = EdgeInsets.zero,
+  });
+
+  final Rect area;
+  final BorderRadius radius;
+  final EdgeInsets padding;
+
+  @override
+  Path getClip(Size size) {
+    final Rect rect = Rect.fromLTRB(
+      area.left - padding.left,
+      area.top - padding.top,
+      area.right + padding.right,
+      area.bottom + padding.bottom,
+    );
+
+    return Path()
+      ..fillType = PathFillType.evenOdd
+      ..addRect(Offset.zero & size)
+      ..addRRect(
+        RRect.fromRectAndCorners(
+          rect,
+          topLeft: radius.topLeft,
+          topRight: radius.topRight,
+          bottomLeft: radius.bottomLeft,
+          bottomRight: radius.bottomRight,
+        ),
+      );
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) =>
+      oldClipper is _HighLighter &&
+          (radius != oldClipper.radius || area != oldClipper.area);
+}
